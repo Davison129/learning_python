1f67cd01f955164e4e0eee6e68ab3885922e9e65
Davison129 <anna.davison@ucdconnect.ie>
2020-10-03 12:52:25 +0100

    default arguments

--- FirstProgram/main.py
+++ FirstProgram/main.py
@@ -1,8 +1,7 @@
 
 
-def my_function(str1, str2):
-    print(str1)
-    print(str2)
+def print_something(name="Someone", age="Unknown"):
+    print("My name is", name, "and my age is", age)
 
-my_function("This is argument 1","This is the second argument which is also a string")
-my_function("Stringy","Hello World")
+
+print_something("Nick")
