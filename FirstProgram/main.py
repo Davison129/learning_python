a59f6204416a4174757c2d7138c7bcbff3e07649
Davison129 <anna.davison@ucdconnect.ie>
2020-11-24 11:44:49 +0000

    return values

--- FirstProgram/main.py
+++ FirstProgram/main.py
@@ -1,8 +1,10 @@
 
 
-def print_people(*people):
-    for person in people:
-        print("This person is", person, ".")
+def do_math(num1, num2):
+    return num1 + num2
 
 
-print_people("Nick", "Andy", "Buzz", "Sara")
+math1 = do_math(5, 7)
+math2 = do_math(11, 34)
+
+print("First sum is", math1, "and the second sum is", math2)
